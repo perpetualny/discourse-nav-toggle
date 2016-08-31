6fff21bb1b903dc08152015013fd75bddc49e7f4
snehesht <mail@snehesh.me>
2016-08-31 17:57:03 -0400

    discourse js injection

--- assets/app.js
+++ assets/app.js
@@ -67,16 +67,24 @@
 // 	}
 // });
 
-(function(){
-setTimeout(function(){
-	console.log('this is the dom');
-	document.getElementsByTagName('html')[0].style['overflow-y'] = 'hidden';
-	document.getElementsByTagName('html')[0].style['background-color'] = '#FCFCFC';
-	document.getElementsByClassName('d-header')[0].style['background-color'] = "#FCFCFC";
-	document.getElementsByClassName('d-header')[0].style['box-shadow'] = 'none';
-	document.getElementsByClassName('d-header')[0].getElementsByClassName('title')[0].style['display']='none';
-},5000);
-})();
+// (function(){
+// setTimeout(function(){
+// 	console.log('this is the dom');
+// 	document.getElementsByTagName('html')[0].style['overflow-y'] = 'hidden';
+// 	document.getElementsByTagName('html')[0].style['background-color'] = '#FCFCFC';
+// 	document.getElementsByClassName('d-header')[0].style['background-color'] = "#FCFCFC";
+// 	document.getElementsByClassName('d-header')[0].style['box-shadow'] = 'none';
+// 	document.getElementsByClassName('d-header')[0].getElementsByClassName('title')[0].style['display']='none';
+// },5000);
+// })();
+
+$(document).ready(function(){
+	$('html')[0].style['overflow-y'] = 'hidden';
+	$('html')[0].style['background-color'] = '#FCFCFC';
+	$('.d-header')[0].style['background-color'] = "#FCFCFC";
+	$('.d-header')[0].style['box-shadow'] = 'none';
+	$('.d-header .title')[0].style.display = "none"
+});
 
 // setTimeout(function(){
 // 	console.log('this is the dom');
