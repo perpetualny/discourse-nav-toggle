eacdcdcbd5f003b4622fdfad182367f36bf95e27
snehesht <mail@snehesh.me>
2016-08-31 17:28:17 -0400

    discourse js injection

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ Discourse.TopicView.reopen({
 		Ember.run.scheduleOnce('afterRender', this, this.afterRenderEvent);
 	},
 	afterRenderEvent : function(){
+		console.log('inside discourse topicview');
 		document.getElementsByTagName('html')[0].style['overflow-y'] = 'hidden'
 		document.getElementsByTagName('html')[0].style['background-color'] = '#FCFCFC'
 		document.getElementsByClassName('d-header')[0].style['background-color'] = "#FCFCFC"
@@ -12,9 +13,10 @@ Discourse.TopicView.reopen({
 	}
 });
 
-
-
-
+(function(){
+	console.log('this is the dom');
+	console.log(document.getElementsByTagName('html')[0]);
+})();
 
 // Discourse.TopicView.reopen({
 // 	didInsertElement : function(){
