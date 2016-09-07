ed2564c4c4c6926fe551b069c488ecf1e3ea5a72
snehesht <mail@snehesh.me>
2016-09-07 11:07:52 -0400

    disabled scrollbar customizations

--- assets/app.js
+++ assets/app.js
@@ -1,6 +1,9 @@
 
 $(document).ready(function(){
-	$('html')[0].style['overflow-y'] = 'hidden';
+	// Disable Scroll bar
+	// $('html')[0].style['overflow-y'] = 'hidden';
+
+	// Strip discourse default styling for header
 	$('html')[0].style['background-color'] = '#FCFCFC';
 	$('.d-header')[0].style['background-color'] = "#FCFCFC";
 	$('.d-header')[0].style['box-shadow'] = 'none';
